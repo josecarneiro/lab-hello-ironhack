2171ce1525ed47eebf005b4666fc13309f797100
Prelhaz <54009534+Prelhaz@users.noreply.github.com>
2019-09-23 15:02:00 +0100

    Done without css

---
+++ starter-code/src/components/HelloHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class HelloHeader extends React.Component {
+  render() {
+    return (
+      <h1>
+        Say hello<br></br>to ReactJs
+      </h1>
+    );
+  }
+}
+
+export default HelloHeader;
---
+++ starter-code/src/components/TagLineParagraph.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TagLine extends React.Component {
+  render() {
+    return (
+      <p>
+        You will learn a Frontend<br></br>framework from scratch, to
+        <br></br>become a NinJa Developer
+      </p>
+    );
+  }
+}
+export default TagLine;
