c8599be878b4e7f1f2aa7e8eebeca9b62f82c147
Tiago Silva <tiagosilva94@outlook.com>
2019-09-23 14:59:12 +0100

    Add most of the components for the app

---
+++ starter-code/src/components/AwesomeButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class AwesomeButton extends React.Component {
+  render() {
+    return <button>Awesome!</button>;
+  }
+}
+
+export default AwesomeButton;
---
+++ starter-code/src/components/IronhackLogo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class IronhackLogo extends React.Component {
+  render() {
+    return;
+  }
+}
+
+export default IronhackLogo;
