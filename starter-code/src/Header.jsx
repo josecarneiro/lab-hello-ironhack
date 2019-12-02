37a3b8c2481b4128484981cc7a1c489b2dfee438
another-dj <alexis_forestier@hotmail.com>
2019-12-02 18:31:51 +0000

    done without styles

---
+++ starter-code/src/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+      <h1>Say hello to ReactJS</h1>
+      <p>You will lear a Frontend framework from scratch, to become a Ninja Developer</p>
+      <button>Awesome!</button>
+      </header>
+    );
+  }
+}
+export default Header;
