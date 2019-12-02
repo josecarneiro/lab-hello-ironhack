16feb5f86268b43edcd9b7ed1ab0c84956eb6f52
mloureiro93 <mariamadureiraloureiro@gmail.com>
2019-12-02 15:16:19 +0000

    not done

---
+++ starter-code/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  render() {
+    return (
+        <button className="Button"> Awesome!</button>
+    );
+  }
+}
+
+export default Button;
