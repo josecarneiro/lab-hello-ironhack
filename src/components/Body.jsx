d69fdcd77c6e46b09806c3df9969fdca00e30055
RaqueCoelho <57578500+RaqueCoelho@users.noreply.github.com>
2020-03-02 13:33:37 +0000

    done

---
+++ src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+function Body() {
+  return (
+    <div classNAme="bodycss">
+      <h1>Say hello to ReactJS</h1>
+      <p>You will learn a Frontend framework from scratch, to become an Ninka Developer</p>
+      <button>Awesome</button>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+function Footer() {
+  return (
+    <div className="iconscontainer">
+      <div className="icons">
+        <img src="images/icon1.png" alt="icon1" />
+        <h4>Declarative</h4>
+        <p>loremnfsfhlwdf jlfkhldf hdslfh</p>
+      </div>
+      <div>
+        <img src="images/icon2.png" alt="icon2" />
+        <h4>Componentes</h4>
+        <p>loremnfsfhlwdf jlfkhldf hdslfh</p>
+      </div>
+      <div>
+        <img src="images/icon3.png" alt="icon3" />
+        <h4>Single-Way</h4>
+        <p>loremnfsfhlwdf jlfkhldf hdslfh</p>
+      </div>
+      <div>
+        <img src="images/icon4.png" alt="icon4" />
+        <h4>JSX</h4>
+        <p>loremnfsfhlwdf jlfkhldf hdslfh</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
