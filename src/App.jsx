814e8e920b4e4307f61af49f9af7fb66a0257f48
FilipeAVNunes <filipenunes407@hotmail.com>
2020-03-02 14:15:17 +0000

    re-arrange the folders and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-
 import './App.css';
 
 class App extends Component {
@@ -8,19 +7,45 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <img
+              className="ironhacklogo"
+              src={process.env.PUBLIC_URL + './images/ironhack-logo.svg'}
+            />
+            <img className="menutop" src={process.env.PUBLIC_URL + './images/menu-top.svg'} />
+          </nav>
+          <h1>
+            <bold>Say hello to ReactJS</bold>
+          </h1>
+          <h3>You will learn a Frontend framework from scratch, to become an Ninja Developer.</h3>
+          <button className="button">Awesome!</button>
         </header>
+
+        <div className="App-body">
+          <section>
+            <img class="declarativeimg" src={process.env.PUBLIC_URL + './images/icon1.png'} />
+            <h5>Declarative</h5>
+            <p>React makes it painless to create interactive UIs.</p>
+          </section>
+
+          <section>
+            <img class="componentsimg" src={process.env.PUBLIC_URL + './images/icon2.png'} />
+            <h5>Components</h5>
+            <p>Build encapsulated components that manage their state.</p>
+          </section>
+
+          <section>
+            <img class="singlewayimg" src={process.env.PUBLIC_URL + './images/icon3.png'} />
+            <h5>Single-Way</h5>
+            <p>A set of immutable values are passed to the component's.</p>
+          </section>
+
+          <section>
+            <img class="JSXimg" src={process.env.PUBLIC_URL + './images/icon4.png'} />
+            <h5>JSX</h5>
+            <p>Statically-typed, designed to run on modern browsers.</p>
+          </section>
+        </div>
       </div>
     );
   }
