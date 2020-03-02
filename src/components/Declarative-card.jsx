3a2281fac0a5d9f4a69e14b36bbaf240632712b5
alinepasetti <pasettialine@gmail.com>
2020-03-02 14:18:06 +0000

    done

---
+++ src/components/Declarative-card.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DeclarativeCard = () => {
+  return (
+    <div>
+      <h4>Declarative</h4>
+      <p>Lorem Ipsum Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum</p>
+    </div>
+  );
+};
+
+export default DeclarativeCard;
