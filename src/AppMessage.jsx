9820ae4da9cb1140fdc780e1695a5d21fd93faaf
Ric Az <ricardoazevedo96@gmail.com>
2020-03-02 14:17:31 +0000

    without background

---
+++ src/AppMessage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AppMessage = () => {
+  return (
+    <div>
+      <h1>
+        Say hello to <br></br> ReactJS
+      </h1>
+      <h3>
+       You will learn a Frontend <br></br> framework from scratch, to <br></br> become an Ninja
+        Developer
+      </h3>
+    </div>
+  );
+};
+export default AppMessage;
