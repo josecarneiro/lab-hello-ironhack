d06ab4ed3d381a5d12c007c8892a35779cd8bdbc
Gabriela Fonseca <gabmfonseca@gmail.com>
2020-03-02 13:06:15 +0000

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,38 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <nav>
+            <img src={process.env.PUBLIC_URL + '/images/ironhack-logo.svg'} alt="Ironhack logo" />
+            <img src={process.env.PUBLIC_URL + '/images/menu-top.svg'} alt="Menu" />
+          </nav>
+          <div className="App-intro">
+            <h1>Say hello to ReactJS</h1>
+            <p>You will learn a Frontend framework from scratch, to become a Ninja Developer.</p>
+            <button>Awesome!</button>
+          </div>
         </header>
+        <main>
+          <div className="card">
+            <img src={process.env.PUBLIC_URL + '/images/icon1.png'} alt="Declarative Icon" />
+            <h2>Declarative</h2>
+            <p>React makes it painless to create interactive UIs.</p>
+          </div>
+          <div className="card">
+            <img src={process.env.PUBLIC_URL + '/images/icon2.png'} alt="Components Icon" />
+            <h2>Components</h2>
+            <p>Build encapsulated components that manage their state.</p>
+          </div>
+          <div className="card">
+            <img src={process.env.PUBLIC_URL + '/images/icon3.png'} alt="Single-Way Icon" />
+            <h2>Single-Way</h2>
+            <p>A set of immutable values are passed to the components.</p>
+          </div>
+          <div className="card">
+            <img src={process.env.PUBLIC_URL + '/images/icon4.png'} alt="JSX Icon" />
+            <h2>JSX</h2>
+            <p>Statically-typed designed to run on modern browsers.</p>
+          </div>
+        </main>
       </div>
     );
   }
